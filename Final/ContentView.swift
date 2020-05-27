28956706e1124790e15a635f51b621c9c1ef02a3
TsuNiWan <tom007880510@gmail.com>
2020-05-27 10:54:19 +0800

    Initial Commit

---
+++ Final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Final
+//
+//  Created by 林子平 on 2020/5/27.
+//  Copyright © 2020 林子平. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
